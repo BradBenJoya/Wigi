46695a077728e0966f511f7e398636ba51a61fb6
BradBenJoya <bradbenjoya6@gmail.com>
2026-07-07 01:29:20 +0300

    Add alpha channel, sf::Color conversion, and comparison operators to Color
    Add alpha channel (default 255) to wigi::Color, a constructor from
    sf::Color for round-tripping, operator== / operator!=, and default
    member initializers so Color() no longer leaves fields uninitialized.

--- include/color.h
+++ include/color.h
@@ -6,17 +6,30 @@
 namespace wigi {
     struct Color {
         using uint8 = std::uint8_t;
-        uint8 r;
-        uint8 g;
-        uint8 b;
+        uint8 r = 0;
+        uint8 g = 0;
+        uint8 b = 0;
+        uint8 a = 255;
 
         Color() = default;
 
-        Color(const uint8 r, const uint8 g, const uint8 b) : r(r), g(g), b(b) {}
+        Color(const uint8 r, const uint8 g, const uint8 b, const uint8 a = 255) : r(r), g(g), b(b), a(a) {}
+
+        Color(const sf::Color& c) : r(c.r), g(c.g), b(c.b), a(c.a) {}
 
         operator sf::Color() const {
-            return sf::Color(r, g, b);
+            return sf::Color(r, g, b, a);
         }
+
+        // Comparison
+        bool operator==(const Color& c) const {
+            return r == c.r && g == c.g && b == c.b && a == c.a;
+        }
+
+        bool operator!=(const Color& c) const {
+            return !(*this == c);
+        }
+
     };
 } // Namespace wigi
 
