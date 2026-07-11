3dc2c6e1d5c8bcd11f6c1dcd9873321b6212a5c3
BradBenJoya <bradbenjoya6@gmail.com>
2026-07-12 01:00:53 +0300

    Finished base class of Dropdown widget

--- include/dropdown.h
+++ include/dropdown.h
@@ -5,7 +5,13 @@
 
 namespace wigi {
     class Dropdown : public Button {
-
+        public:
+            Dropdown(unsigned int width, unsigned int height, unsigned int items = 0);
+        private:
+            Button m_selectedButton;
+            std::vector<Button> m_items;
+            void dropDown();
+            void selectFromDropDown();
     };
 }
 
